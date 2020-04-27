f639261cda610960e0a9e4dd6d77ff05e145e8c6
cristianrz <dev@cristianrz.com>
2020-04-27 19:03:38 +0100

    Fix resolv.conf not properly copied

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ _bootstrap_error() {
 }
 
 _bootstrap() {
-	[ ! -d "$POCKER_IMAGES/$1" ] && sh "$POCKER_LIBS/pull.sh" "$1"
+	[ ! -d "$POCKER_IMAGES/$1" ] && _pull "$1"
 
 	mkdir -p "$2"/rootfs
 
@@ -19,6 +19,6 @@ _bootstrap() {
 
 	sh "$POCKER_IMAGES/$1/bootstrap.sh" || true
 	echo "export PS1='\u@$(basename "$2"):\w\\$ '" >>etc/profile
-	rm etc/resolv.conf
-	cat /etc/resolv.conf etc/resolv.conf
+	rm -f etc/resolv.conf
+	cat /etc/resolv.conf > etc/resolv.conf
 }
