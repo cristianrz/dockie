6b76ca53b10bde02f716239085ba5d472c6d0633
cristianrz <dev@cristianrz.com>
2020-05-06 17:37:59 +0100

    Remove old comment

--- ls.sh
+++ ls.sh
@@ -4,10 +4,8 @@
 # List rootfs
 #
 _ls() {
-	[ "$#" -ne 0 ] && _print_usage "ls"
+	[ "$#" ] || _print_usage "ls"
 
-	# posix built-in way of checking if directory is empty, might fail if
-	# there is a single file called '*' but you should probably not do that
 	{
 		echo "ROOTFS ID,IMAGE,CREATED,NAME"
 		cat "$DOCKIE_GUESTS"/*/info 2>/dev/null
