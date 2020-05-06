96617fb8fa454e016add9e357b02c6aba9094c43
cristianrz <dev@cristianrz.com>
2020-05-06 20:43:37 +0100

    Fix wrong condition

--- ls.sh
+++ ls.sh
@@ -4,12 +4,12 @@
 # List rootfs
 #
 _ls() {
-	[ "$#" ] || _print_usage "ls"
+	[ "$#" -eq 0 ] || _print_usage "ls"
 
 	{
 		echo "ROOTFS ID,IMAGE,CREATED,NAME"
 		cat "$DOCKIE_GUESTS"/*/info 2>/dev/null
-	} | awk -F ',' '{printf "%-15s%-15s%-25s%s\n",$1,$2,$3,$4}'
+	} | awk -F ',' '{printf "%-12s%-15s%-25s%s\n",$1,$2,$3,$4}'
 }
 
 _ps() {
