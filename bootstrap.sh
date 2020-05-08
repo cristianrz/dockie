c16d7c5d0c3bbcb805b1b9d41d7040c4f03c0488
cristianrz <dev@cristianrz.com>
2020-05-08 13:45:05 +0100

    Only add DISPLAY if it is set
    Signed-off-by: cristianrz <dev@cristianrz.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,20 @@
 # shellcheck shell=sh
+
+# _bootstrap_config(system, id, name)
 _bootstrap_config() {
-	guest_path="$2"
-	guest_name="$(_strings_basename "$guest_path")"
+	guest_name="$3"
+
+	guest_path="$DOCKIE_GUESTS/$2"
 	guest_prefix="$guest_path/rootfs"
 
-	id="$(cat /proc/sys/kernel/random/uuid)"
-	id="${id%%-*}"
 
-	echo "$id,$guest_name,$(_date),$1" > "$guest_path/info"
+	echo "$id,$1,$(_date),$guest_name" > "$guest_path/info"
 	echo "$id"
 
 	{
 		echo
 		echo "# added by dockie"
-		echo "export DISPLAY='$DISPLAY'"
+		[ -n "${DISPLAY-}" ] && echo "export DISPLAY='$DISPLAY'"
 		# not sure why PATH is not exported by default
 		echo "export PATH"
 		echo "export PS1='\033[30;34m\u@$guest_name \w \\$ \033[30;39m'"
@@ -24,11 +25,12 @@ _bootstrap_config() {
 
 }
 
+# _bootstrap(system, id, name)
 _bootstrap() {
-	echo "Bootstrapping... This may take a few minutes depending on your" \
-		"system."
+	# echo "Bootstrapping... This may take a few minutes depending on your" \
+	# "system."
 	system="$1"
-	guest_path="$2"
+	guest_path="$DOCKIE_GUESTS/$2"
 	guest_prefix="$guest_path/rootfs"
 
 	[ ! -d "$DOCKIE_IMAGES/$system" ] && _pull "$system"
