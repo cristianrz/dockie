a43c95212eef66b4d83cb5401207b1965d2fe249
cristianrz <dev@cristianrz.com>
2020-05-09 19:44:26 +0100

    Remove _strings_basename as its only used once
    Signed-off-by: cristianrz <dev@cristianrz.com>

--- log.sh
+++ log.sh
@@ -1,5 +1,5 @@
 # shellcheck shell=sh
 _log_fatal() {
-	printf '\033[30;31m%s:\033[30;39m %s\n' "$(_strings_basename "$0")" "$*" >&2
+	printf '\033[30;31m%s:\033[30;39m %s\n' "$(basename "$0")" "$*" >&2
 	exit 1
 }
--- strings.sh
+++
@@ -1,6 +0,0 @@
-# shellcheck shell=sh
-_strings_basename() {
-	dir=${1%${1##*[!/]}}
-	dir=${dir##*/}
-	printf '%s\n' "${dir:-/}"
-}
