1e36db39e0c512fe4a70ad0a0cacbb5f439a3f52
Cristian Ariza <dev@cristianrz.com>
2020-05-11 01:59:53 +0100

    Make case a one liner
    Signed-off-by: Cristian Ariza <dev@cristianrz.com>

--- strings.sh
+++ strings.sh
@@ -2,8 +2,5 @@
 
 # _strings_contains(string, substring)
 _strings_contains() {
-	case x"$1" in
-	*"$2"*) return 0 ;;
-	*) return 1 ;;
-	esac
+	case x"$1" in *"$2"*) return 0 ;; *) return 1 ;; esac
 }
