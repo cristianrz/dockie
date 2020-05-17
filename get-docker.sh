e211b7866b16634700674b0ee08083d3d8829706
Cristian Ariza <dev@cristianrz.com>
2020-05-17 11:08:10 +0100

    Execute docker-hub-pull relative to HERE
    Signed-off-by: Cristian Ariza <dev@cristianrz.com>

--- get-docker.sh
+++ get-docker.sh
@@ -5,7 +5,7 @@ _get() {
 	trap 'rm -rf "$TMP"' EXIT
 
 	echo "Pulling from Docker Hub..."
-	docker-hub-pull "$TMP" "$2" >/dev/null || return 1
+	bash "$HERE/../lib/dockie/docker-hub-pull" "$TMP" "$2" >/dev/null || return 1
 
 	# shellcheck disable=SC2012
 	mv "$(ls -1S "$TMP"/*/layer.tar | head -n 1)" "$1/rootfs.tar.gz"
